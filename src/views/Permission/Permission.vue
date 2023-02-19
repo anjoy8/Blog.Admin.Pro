a199eff3b12084a62f96171d48f57bb364c0f68c
zhanganzhong <3143422472@qq.com>
2023-02-19 22:31:53 +0800

    feat: :o2:  add permission part

--- src/views/Permission/Permission.vue
+++ src/views/Permission/Permission.vue
@@ -4,37 +4,58 @@
     <!-- <toolbar :buttonList="buttonList" @callFunction="callFunction"></toolbar> -->
 
     <!--列表-->
-    <el-table :data="users" v-loading="listLoading" style="width: 100%" ref="table" @select="dialogCheck"
-      @row-click="selectCurrentRow" class="custom-tbl">
-      <el-table-column type="selection" width="50"> </el-table-column>
-      <el-table-column type="index" width="80"> </el-table-column>
-      <el-table-column prop="LinkUrl" label="接口地址" width="" sortable>
-      </el-table-column>
-      <el-table-column prop="Name" label="描述" width="300" sortable>
-      </el-table-column>
-      <el-table-column prop="CreateTime" label="创建时间" :formatter="formatCreateTime" width="" sortable>
-      </el-table-column>
-      <el-table-column prop="CreateBy" label="创建者" width="" sortable>
-      </el-table-column>
-      <el-table-column prop="Enabled" label="状态" width="" sortable>
+    <el-table :data="users" v-loading="listLoading" row-key="Id" border lazy :load="load"
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }" style="width: 100%" ref="table"
+      @select="dialogCheck" @row-click="selectCurrentRow" class="custom-tbl">
+      <el-table-column type="selection" width="50"></el-table-column>
+      <el-table-column type="index" width="80"></el-table-column>
+      <el-table-column label="菜单/按钮" width="200">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.Enabled ? 'success' : 'danger'" disable-transitions>{{ scope.row.Enabled ? "正常" : "禁用"
-          }}
-          </el-tag>
+          <i class="fa" :class="scope.row.Icon"></i>
+
+          {{ scope.row.Name }}
         </template>
       </el-table-column>
-
+      <el-table-column prop="Code" label="路由地址" width></el-table-column>
+      <el-table-column prop="MName" label="API接口" width></el-table-column>
+      <el-table-column prop="OrderSort" label="Sort" width></el-table-column>
+      <el-table-column prop="IsButton" label="是否按钮" width="100">
+        <template slot-scope="scope">
+          <el-tag :type="!scope.row.IsButton ? 'success' : 'danger'" disable-transitions>{{ !scope.row.IsButton ? "否" :
+            "是" }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="Func" label="按钮事件" width></el-table-column>
+      <el-table-column prop="IsHide" label="是否隐藏" width="100">
+        <template slot-scope="scope">
+          <el-tag :type="!scope.row.IsHide ? 'success' : 'danger'" disable-transitions>{{ !scope.row.IsHide ? "否" : "是"
+          }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="IskeepAlive" label="keepAlive" width="100">
+        <template slot-scope="scope">
+          <el-tag :type="!scope.row.IskeepAlive ? 'success' : 'danger'" disable-transitions>{{ !scope.row.IskeepAlive ?
+            "否" : "是" }}</el-tag>
+        </template>
+      </el-table-column>
     </el-table>
+
+
+
   </section>
 </template>
 
 <script>
 import util from "@/utils/date";
 import {
+  getPermissionListPage,
+  getPermissionTreeTable,
+  removePermission,
+  editPermission,
+  addPermission,
+  getPermissionTree,
   getModuleListPage,
-  removeModule,
-  editModule,
-  addModule,
+  migratePermissionTable
 } from "@/api/api";
 import { getButtonList } from "@/permission";
 import Toolbar from "../../components/Toolbar";
@@ -103,14 +124,14 @@ export default {
       rowSelection,
       expandedRowKeys: [],
       selectedRowKeys: [],
-
       filters: {
-        LinkUrl: "",
+        Name: "",
       },
       users: [],
+      modules: [], //接口api列表
       statusList: [
-        { LinkUrl: "激活", value: true },
-        { LinkUrl: "禁用", value: false },
+        { Name: "激活", value: true },
+        { Name: "禁用", value: false },
       ],
       total: 0,
       page: 1,
@@ -121,34 +142,54 @@ export default {
       editFormVisible: false, //编辑界面是否显示
       editLoading: false,
       editFormRules: {
-        LinkUrl: [
-          { required: true, message: "请输入接口地址", trigger: "blur" },
-        ],
+        Name: [{ required: true, message: "请输入菜单名称", trigger: "blur" }],
+        Code: [{ required: true, message: "请输入路由地址", trigger: "blur" }],
       },
       //编辑界面数据
       editForm: {
         Id: 0,
+        Mid: 0,
+        OrderSort: 0,
+        PidArr: [],
         CreateBy: "",
-        LinkUrl: "",
         Name: "",
-        Enabled: false,
+        Code: "",
+        Description: "",
+        Icon: "",
+        Func: "",
+        Enabled: true,
+        IsButton: false,
+        IsHide: false,
+        IskeepAlive: false,
       },
 
       addFormVisible: false, //新增界面是否显示
       addLoading: false,
+      addCodeDisabled: false,
+      editCodeDisabled: false,
+      syncFormVisible: false,
       addFormRules: {
-        LinkUrl: [
-          { required: true, message: "请输入接口地址", trigger: "blur" },
-        ],
+        Name: [{ required: true, message: "请输入菜单名称", trigger: "blur" }],
+        Code: [{ required: true, message: "请输入路由地址", trigger: "blur" }],
       },
       //新增界面数据
       addForm: {
         CreateBy: "",
         CreateId: "",
-        LinkUrl: "",
+        PidArr: [],
+        Mid: 0,
+        OrderSort: 0,
         Name: "",
-        Enabled: "",
+        Code: "",
+        Description: "",
+        Icon: "",
+        Func: "",
+        Enabled: true,
+        IsButton: false,
+        IsHide: false,
+        IskeepAlive: false,
       },
+      isResouceShow: 0,
     };
   },
   methods: {
@@ -169,6 +210,34 @@ export default {
         this.$refs.table.toggleRowSelection(val, true);
       }
     },
+    clkType() {
+      this.addForm.IsButton = false;
+      this.addCodeDisabled = false;
+      if (this.addForm.MenuType == "页面") {
+        this.addForm.Code = "";
+      } else if (this.addForm.MenuType == "目录") {
+        this.addForm.Code = "-";
+        this.addCodeDisabled = true;
+      } else if (this.addForm.MenuType == "按钮") {
+        this.addForm.Code = " ";
+        this.addForm.IsButton = true;
+        this.addCodeDisabled = true;
+      }
+    },
+    clkTypeEdit() {
+      this.editForm.IsButton = false;
+      this.editCodeDisabled = false;
+      if (this.editForm.MenuType == "页面") {
+        this.editForm.Code = "";
+      } else if (this.editForm.MenuType == "目录") {
+        this.editForm.Code = "-";
+        this.editCodeDisabled = true;
+      } else if (this.editForm.MenuType == "按钮") {
+        this.editForm.Code = " ";
+        this.editForm.IsButton = true;
+        this.editCodeDisabled = true;
+      }
+    },
     callFunction(item) {
       this.filters = {
         name: item.search,
@@ -186,10 +255,20 @@ export default {
     },
     handleCurrentChange(val) {
       this.page = val;
-      this.getModules();
+      this.getPermissions();
+    },
+    load(tree, treeNode, resolve) {
+      let para = {
+        page: this.page,
+        f: tree.Id,
+        key: this.filters.Name,
+      };
+      getPermissionTreeTable(para).then((res) => {
+        resolve(res.data.response);
+      });
     },
     //获取用户列表
-    getModules() {
+    getPermissions() {
       let para = {
         page: this.page,
         key: this.filters.name,
@@ -197,9 +276,8 @@ export default {
       this.listLoading = true;
 
       //NProgress.start();
-      getModuleListPage(para).then((res) => {
-        this.total = res.data.response.dataCount;
-        this.users = res.data.response.data;
+      getPermissionTreeTable(para).then((res) => {
+        this.users = res.data.response;
         this.listLoading = false;
         //NProgress.done();
       });
@@ -222,7 +300,7 @@ export default {
           this.listLoading = true;
           //NProgress.start();
           let para = { id: row.Id };
-          removeModule(para).then((res) => {
+          removePermission(para).then((res) => {
             if (util.isEmt.format(res)) {
               this.listLoading = false;
               return;
@@ -241,7 +319,7 @@ export default {
               });
             }
 
-            this.getModules();
+            this.getPermissions();
           });
         })
         .catch(() => { });
@@ -257,18 +335,82 @@ export default {
 
         return;
       }
-      this.editFormVisible = true;
-      this.editForm = Object.assign({}, row);
+      let that = this;
+
+      that.options = [];
+
+      this.editForm = {};
+      that.editLoading = true;
+      that.editFormVisible = true;
+      that.editCodeDisabled = false;
+      let para = { pid: row.Id };
+      getPermissionTree(para).then((res) => {
+        ++that.isResouceShow;
+        this.options.push(res.data.response);
+        that.editForm = Object.assign({}, row);
+        that.editLoading = false;
+      });
+    },
+    //显示同步界面
+    handleSync(isAction = false) {
+      let row = this.currentRow;
+      var pid = row && row.Id;
+      if (!(pid > 0)) {
+        this.$message({
+          message: "请选择要同步的父级页面！",
+          type: "error",
+        });
+
+        return;
+      }
+
+      var controllerName = row && row.MName && row.MName.split('/')[2] || '';
+      if (!controllerName) {
+        this.$message({
+          message: "要同步的接口控制器名不能为空！",
+          type: "error",
+        });
+
+        return;
+      }
+
+      let that = this;
+
+      that.syncoptions = [];
+
+      that.syncFormVisible = true;
+      let para = { controllerName, pid, isAction };
+      migratePermissionTable(para).then((res) => {
+        that.syncoptions = res.data.response;
+      });
     },
     //显示新增界面
     handleAdd() {
+      this.options = [];
       this.addFormVisible = true;
+      this.addCodeDisabled = false;
+      this.addLoading = true;
       this.addForm = {
         CreateBy: "",
-        LinkUrl: "",
+        CreateId: "",
+        PidArr: [],
         Name: "",
-        Enabled: "true",
+        Code: "",
+        OrderSort: 0,
+        Description: "",
+        Enabled: true,
+        Icon: "",
+        IsButton: false,
+        IsHide: false,
+        IskeepAlive: false,
       };
+
+      let para = { pid: 0 };
+      getPermissionTree(para).then((res) => {
+        ++this.isResouceShow;
+        this.options.push(res.data.response);
+        this.addLoading = false;
+      });
     },
     //编辑
     editSubmit: function () {
@@ -281,11 +423,24 @@ export default {
 
             para.ModifyTime = util.formatDate.format(new Date(), "yyyy-MM-dd");
 
-            editModule(para).then((res) => {
+            para.Pid = para.PidArr.pop();
+
+            if (para.Id == para.Pid) {
+              this.$message({
+                message: "警告，父节点不能是自己！",
+                type: "error",
+              });
+
+              this.editLoading = false;
+              return;
+            }
+
+            editPermission(para).then((res) => {
               if (util.isEmt.format(res)) {
                 this.editLoading = false;
                 return;
               }
+              this.editLoading = false;
               if (res.data.success) {
                 this.editLoading = false;
                 //NProgress.done();
@@ -294,8 +449,9 @@ export default {
                   type: "success",
                 });
                 this.$refs["editForm"].resetFields();
+                this.$refs.table.setCurrentRow();
                 this.editFormVisible = false;
-                this.getModules();
+                this.getPermissions();
               } else {
                 this.$message({
                   message: res.data.msg,
@@ -321,6 +477,8 @@ export default {
             para.ModifyTime = para.CreateTime;
             para.IsDeleted = false;
 
+            para.Pid = para.PidArr.pop();
+
             var user = JSON.parse(window.localStorage.user);
 
             if (user && user.uID > 0) {
@@ -336,12 +494,12 @@ export default {
               );
             }
 
-            addModule(para).then((res) => {
+            addPermission(para).then((res) => {
               if (util.isEmt.format(res)) {
                 this.addLoading = false;
                 return;
               }
-
+              this.addLoading = false;
               if (res.data.success) {
                 this.addLoading = false;
                 //NProgress.done();
@@ -350,8 +508,10 @@ export default {
                   type: "success",
                 });
                 this.$refs["addForm"].resetFields();
+                this.options = [];
+                this.$refs.table.setCurrentRow();
                 this.addFormVisible = false;
-                this.getModules();
+                this.getPermissions();
               } else {
                 this.$message({
                   message: res.data.msg,
@@ -365,7 +525,11 @@ export default {
     },
   },
   mounted() {
-    this.getModules();
+    this.getPermissions();
+
+    getModuleListPage({ page: -1 }).then((res) => {
+      this.modules = res.data.response.data;
+    });
 
     let routers = window.localStorage.router
       ? JSON.parse(window.localStorage.router)
